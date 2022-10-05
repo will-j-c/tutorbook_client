1257b0d5721150f61026c89fd2df7de40d8175c6
Will <willjcGA@outlook.com>
2022-10-05 23:40:37 +0800

    feat: added tailwind setup and other devtools

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-tertiary">
+      Hello
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      primary: '#EFEFEF',
+      secondary: '#CAEBF2',
+      tertiary: '#FF3B3F',
+      titleText: '#A9A9A9',
+      bodyText: '#ecc94b'
+    },
+  },
+  plugins: [],
+}
