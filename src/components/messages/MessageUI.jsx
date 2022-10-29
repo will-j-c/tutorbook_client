c36336c244bec1ab95df32043c2e4d10883c1268
Will <willjcGA@outlook.com>
2022-10-29 14:05:23 +0800

    feat: added posting a messafe and basic scrolling and editing behaviour

--- src/components/messages/MessageUI.jsx
+++ src/components/messages/MessageUI.jsx
@@ -1,5 +1,5 @@
 import { useCookies } from 'react-cookie';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { toast } from 'react-toastify';
 import axios from '../../api/axios';
 import Avatar from '../avatars/Avatar';
@@ -11,11 +11,14 @@ function MessageUI() {
   const [data, setData] = useState('');
   const [activeThread, setActiveThread] = useState(null);
   const [activeUUID, setActiveUUID] = useState(null);
-
+  const [newMessage, setNewMessage] = useState('');
+  const messageText = useRef();
+  const messageWindow = useRef();
   const callMessageUserRoute = async (route) => {
     axios.get(route, { headers: { Authorization: `Bearer ${cookies.idToken}` } }).then(
       (response) => {
         setData(response.data);
+        messageWindow.current.scrollTop = messageWindow.current.scrollHeight;
         return;
       },
       (error) => {
@@ -23,6 +26,47 @@ function MessageUI() {
         return;
       }
     );
+  };
+
+  const postMessage = (message) => {
+    const body = {
+      tutor: activeThread.tutor.id,
+      user: activeThread.user.id,
+      thread: activeThread.id,
+      content: newMessage,
+      sender: data.user
+    };
+    axios
+      .post(`messages/${activeThread.thread_uuid}`, body, {
+        headers: { Authorization: `Bearer ${cookies.idToken}` }
+      })
+      .then(
+        (response) => {
+          callMessageUserRoute(`messages`);
+
+          return;
+        },
+        (error) => {
+          console.log(error);
+          toast.error(error?.response?.data.detail || 'Server error');
+          return;
+        }
+      );
+  };
+
+  const handleTypeMessage = (event) => {
+    event.preventDefault();
+    setNewMessage(event.target.value);
+  };
+
+  const sendMessage = () => {
+    if (!newMessage) {
+      return;
+    }
+    messageText.current.value = '';
+    console.log(messageWindow)
+    messageWindow.current.scrollTop = messageWindow.current.scrollHeight;
+    postMessage(newMessage);
   };
 
   const setActive = (event) => {
@@ -46,13 +90,13 @@ function MessageUI() {
   useEffect(() => {
     callMessageUserRoute(`messages`);
   }, []);
-  console.log(activeThread)
+
   return (
     <div className="container mx-auto mt-5">
       <div className="min-w-full border rounded lg:grid lg:grid-cols-3">
         <div className="border-r border-gray-300 lg:col-span-1">
           <div className="mx-3 my-3">
-            <div className="relative text-gray-600">
+            <div className="relative">
               <span className="absolute inset-y-0 left-0 flex items-center pl-2">
                 <svg
                   fill="none"
@@ -75,7 +119,7 @@ function MessageUI() {
             </div>
           </div>
 
-          <h2 className="my-2 mb-2 ml-2 text-lg text-gray-600">Chats</h2>
+          <h2 className="my-2 mb-2 ml-2 text-lg">Chats</h2>
 
           {!data
             ? ''
@@ -95,19 +139,34 @@ function MessageUI() {
             {activeThread ? (
               <>
                 <div className="relative flex items-center p-3 border-b border-gray-300">
-                  <Avatar profile_img_url={data.user === 'u' ? activeThread.tutor.user.profile_img_url : activeThread.user.profile_img_url} size={'h-12 w-12'}/>
-                  <span className="block ml-2 font-bold text-gray-600">{data.user === 'u' ? activeThread.tutor.user.first_name : activeThread.user.first_name}</span>
-                  <span className="absolute w-3 h-3 bg-green-600 rounded-full left-10 top-3"></span>
+                  <Avatar
+                    profile_img_url={
+                      data.user === 'u'
+                        ? activeThread.tutor.user.profile_img_url
+                        : activeThread.user.profile_img_url
+                    }
+                    size={'h-12 w-12'}
+                  />
+                  <span className="block ml-2 font-bold">
+                    {data.user === 'u'
+                      ? activeThread.tutor.user.first_name
+                      : activeThread.user.first_name}
+                  </span>
+                  <span className="absolute w-3 h-3 rounded-full left-10 top-3"></span>
                 </div>
 
-                <div className="relative w-full p-6 overflow-y-auto h-[40rem]">
-                  <ul className="space-y-2">{activeThread?.messages.map(message => {
-                    return <Message key={message.id} data={{message, user: data.user}}/>
-                  })}</ul>
+                <div className="relative w-full p-6 overflow-y-scroll h-[40rem]" ref={messageWindow}>
+                  <ul className="space-y-2">
+                    {activeThread?.messages.map((message) => {
+                      return <Message key={message.id} data={{ message, user: data.user }} />;
+                    })}
+                  </ul>
                 </div>
 
                 <div className="flex items-center justify-between w-full p-3 border-t border-gray-300">
                   <input
+                    onChange={handleTypeMessage}
+                    ref={messageText}
                     type="text"
                     placeholder="Message"
                     className="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700"
@@ -115,7 +174,7 @@ function MessageUI() {
                     required
                   />
 
-                  <button type="submit">
+                  <button type="submit" onClick={sendMessage}>
                     <svg
                       className="w-5 h-5 text-gray-500 origin-center transform rotate-90"
                       xmlns="http://www.w3.org/2000/svg"
